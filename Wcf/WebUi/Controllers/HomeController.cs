ee70cb6c2f8ec57db5e966ef9ad5414cedfe659b
Andy Grant <andrew.grant@codecomputerlove.com>
2018-03-13 14:29:55 +0000

    Web UI for poking service

---
+++ Wcf/WebUi/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+using WebUi.WidgetSvc;
+
+namespace WebUi.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Order order)
+        {
+            using (var service = new OrderingServiceClient("BasicHttpBinding_IOrderingService"))
+            {
+                PlacedOrder result = service.PlaceOrder(order);
+                return View("OrderPlaced", result);
+            }
+        }
+    }
+}
